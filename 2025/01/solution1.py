1a07da0fcdfdeda5065546aefb2a76e234411a96
Andrew Lee <andrew@andrewlee.co.uk>
2025-12-01 16:10:02 +0000

    Solution to day 1

---
+++ 2025/01/solution1.py
@@ -0,0 +1,36 @@
+﻿from operator import floordiv
+
+file = open("input.txt", "r")
+lines = file.read().splitlines()
+
+def text_to_turn_value(text):
+    value = int(text[1:])
+    if text.startswith("L"):
+        return -value
+    return value
+
+revolutions = 100
+position = 50
+count_zero = 0
+count_zero_any = 0
+
+for line in lines:
+    delta = text_to_turn_value(line)
+
+    if position == 0 and delta < 0:
+        position += revolutions
+
+    position += delta
+
+    if position >= revolutions:
+        count_zero_any += position // revolutions
+    elif position <= 0:
+        count_zero_any -= (position - 1) // revolutions
+
+    position %= revolutions
+
+    if position == 0:
+        count_zero += 1
+
+print("Times landed on zero: %d" % count_zero)
+print("Times past zero: %d" % count_zero_any)
