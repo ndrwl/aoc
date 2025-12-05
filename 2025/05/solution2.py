aabee0d4297720a0f42014696fb28ec0d400524b
Andrew Lee <andrew@andrewlee.co.uk>
2025-12-05 11:10:55 +0000

    Solution to day 5

---
+++ 2025/05/solution2.py
@@ -0,0 +1,31 @@
+﻿file = open("input.txt", "r")
+lines = file.read().splitlines()
+
+split_idx = lines.index('')
+
+ranges = []
+
+def add_range(ranges, new_start, new_end):
+    range_idx = 0
+    while range_idx < len(ranges):
+        start, end = ranges[range_idx]
+        if new_end < start or new_start > end:
+            range_idx += 1
+        else:
+            new_start = min(new_start, start)
+            new_end = max(new_end, end)
+            ranges.pop(range_idx)
+    ranges.append((new_start, new_end))
+
+for range_str in lines[:split_idx]:
+    parts = range_str.split("-")
+    start = int(parts[0])
+    end = int(parts[1])
+    add_range(ranges, start, end)
+
+num_ingredients = 0
+
+for start, end in ranges:
+    num_ingredients += end - start + 1
+
+print("Number of fresh ingredients: %d" % num_ingredients)
