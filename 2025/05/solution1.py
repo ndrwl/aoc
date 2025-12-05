ec585a2a5b4639d1dbc8c2e19f0cf65ebff7efca
Andrew Lee <andrew@andrewlee.co.uk>
2025-12-05 11:10:55 +0000

    Solution to day 5

---
+++ 2025/05/solution1.py
@@ -0,0 +1,22 @@
+﻿file = open("input.txt", "r")
+lines = file.read().splitlines()
+
+split_idx = lines.index('')
+
+ranges = []
+num_fresh = 0
+
+for range_str in lines[:split_idx]:
+    parts = range_str.split("-")
+    start = int(parts[0])
+    end = int(parts[1])
+    ranges.append((start, end))
+
+for ingredient_str in lines[split_idx+1:]:
+    ingredient = int(ingredient_str)
+    for start, end in ranges:
+        if start <= ingredient <= end:
+            num_fresh += 1
+            break
+
+print("Number of fresh ingredients: %d" % num_fresh)
