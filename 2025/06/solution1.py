cfc3f14a626cb4805f471c208707453329f7a18c
Andrew Lee <andrew@andrewlee.co.uk>
2025-12-07 20:41:46 +0000

    Solution to day 6

---
+++ 2025/06/solution1.py
@@ -0,0 +1,22 @@
+﻿file = open("input.txt", "r")
+lines = file.read().splitlines()
+
+numbers = [[int(x.strip()) for x in line.strip().split(" ") if x.strip() != ''] for line in lines[:-1]]
+symbols = [x.strip() for x in lines[-1].strip().split(" ") if x.strip() != '']
+
+total = 0
+
+for i in range(len(symbols)):
+    symbol = symbols[i]
+    if symbol == "*":
+        sub_total = 1
+        for row in range(len(numbers)):
+            sub_total *= numbers[row][i]
+        total += sub_total
+    elif symbol == "+":
+        sub_total = 0
+        for row in range(len(numbers)):
+            sub_total += numbers[row][i]
+        total += sub_total
+
+print("Total result: %d" % total)
---
+++ 2025/06/solution2.py
@@ -0,0 +1,55 @@
+﻿file = open("input.txt", "r")
+lines = file.read().splitlines()
+
+def safe_int(s):
+    s = s.strip()
+    if len(s) == 0:
+        return 0
+    return int(s)
+
+def try_get_number(lines, col):
+    number = 0
+    any_number = False
+
+    for row in range(len(lines) - 1):
+        if col < 0 or col >= len(lines[row]):
+            continue
+        if lines[row][col] == ' ':
+            continue
+        number = number * 10 + safe_int(lines[row][col])
+        any_number = True
+
+    return any_number, number
+
+def get_symbol(lines, col):
+    if col < 0 or col >= len(lines[-1]):
+        return ''
+    return lines[-1][col]
+
+max_col = max(len(lines[col]) for col in range(len(lines)))
+
+total = 0
+sub_total = 0
+current_operator = '*'
+
+for col in range(max_col + 1):
+    symbol = get_symbol(lines, col)
+
+    if symbol == '*':
+        current_operator = '*'
+        sub_total = 1
+    elif symbol == '+':
+        current_operator = '+'
+        sub_total = 0
+
+    has_number, number = try_get_number(lines, col)
+    if not has_number:
+        total += sub_total
+        sub_total = 0
+        current_operator = ''
+    elif current_operator == '*':
+        sub_total *= number
+    elif current_operator == '+':
+        sub_total += number
+
+print("Total result: %d" % total)
