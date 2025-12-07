435ef75ff26314df72508310053f4398030ad147
Andrew Lee <andrew@andrewlee.co.uk>
2025-12-07 20:56:38 +0000

    Solution to day 7

---
+++ 2025/07/solution1.py
@@ -0,0 +1,25 @@
+﻿from collections import defaultdict
+
+file = open("input.txt", "r")
+lines = file.read().splitlines()
+
+current_beam = {lines[0].index('S') : 1}
+current_index = 2
+splits = 0
+
+while current_index < len(lines):
+    new_beam = defaultdict(int)
+
+    for pos in current_beam:
+        if lines[current_index][pos] == '^':
+            new_beam[pos + 1] += current_beam[pos]
+            new_beam[pos - 1] += current_beam[pos]
+            splits += 1
+        else:
+            new_beam[pos] += current_beam[pos]
+
+    current_beam = new_beam
+    current_index += 2
+
+print("Number of splits: %d" % splits)
+print("Number of timelines: %d" % sum(current_beam.values()))
