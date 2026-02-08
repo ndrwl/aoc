939d62fdf542144a1874c2715f1b8493d3bc542c
Andrew Lee <andrew@andrewlee.co.uk>
2026-02-08 17:40:05 +0000

    Solution to 2023-06

---
+++ 2023/06/solution1.py
@@ -0,0 +1,23 @@
+﻿file = open("input.txt", "r")
+lines = file.read().splitlines()
+
+times = [int(x) for x in lines[0].split(' ')[1:] if x.strip() != '']
+distances = [int(x) for x in lines[1].split(' ')[1:] if x.strip() != '']
+
+def calculate_distance(speed, time):
+    return speed * time
+
+def calculate_competitive_solutions(total_time, distance_to_beat):
+    competitive_solutions = 0
+    for speed in range(total_time):
+        time_remaining = total_time - speed
+        if calculate_distance(speed, time_remaining) > distance_to_beat:
+            competitive_solutions += 1
+    return competitive_solutions
+
+competitive_solutions = [calculate_competitive_solutions(times[idx], distances[idx]) for idx in range(len(times))]
+
+total_competitive_solutions = 1
+for competitive_solution in competitive_solutions:
+    total_competitive_solutions *= competitive_solution
+print(total_competitive_solutions)
---
+++ 2023/06/solution2.py
@@ -0,0 +1,20 @@
+﻿import math
+
+file = open("input.txt", "r")
+lines = file.read().splitlines()
+
+time = int(''.join([x for x in lines[0].split(' ')[1:] if x.strip() != '']))
+distances = int(''.join([x for x in lines[1].split(' ')[1:] if x.strip() != '']))
+
+# We are solving for the equation 0 > x^2 - tx + d, where t is time and d is distance. We can use the quadratic formula
+# to find the roots of this equation.
+
+discriminant = time**2 - 4 * distances
+solution1 = (time + discriminant**0.5) / 2
+solution2 = (time - discriminant**0.5) / 2
+
+print("Minimum speed: %d" % math.floor(min(solution1, solution2) + 1))
+print("Maximum speed: %d" % math.ceil(max(solution1, solution2) - 1))
+
+range = math.ceil(max(solution1, solution2) - 1) - math.floor(min(solution1, solution2) + 1) + 1
+print("Range of speeds: %d" % range)
