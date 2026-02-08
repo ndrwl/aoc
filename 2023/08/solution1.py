15e736af2670103098008c260159dce04050854d
Andrew Lee <andrew@andrewlee.co.uk>
2026-02-08 18:37:55 +0000

    Solution to day 8

---
+++ 2023/08/solution1.py
@@ -0,0 +1,16 @@
+﻿file = open("input.txt", "r")
+lines = file.read().splitlines()
+
+directions = lines[0]
+
+# parse each line as AAA = (BBB, CCC) into a dictionary of the form {AAA: (BBB, CCC)}
+mapping = {line[0:3]: (line[7:10], line[12:15]) for line in lines[2:]}
+
+instructions = 0
+node = "AAA"
+
+while node != "ZZZ":
+    node = mapping[node][0] if directions[instructions % len(directions)] == "L" else mapping[node][1]
+    instructions += 1
+
+print("Instructions: %d" % instructions)
---
+++ 2023/08/solution2.py
@@ -0,0 +1,35 @@
+﻿file = open("input.txt", "r")
+lines = file.read().splitlines()
+
+directions = lines[0]
+
+# parse each line as AAA = (BBB, CCC) into a dictionary of the form {AAA: (BBB, CCC)}
+mapping = {line[0:3]: (line[7:10], line[12:15]) for line in lines[2:]}
+nodes = [node for node in mapping.keys() if node[2] == 'A']
+
+def length_to_znode(starting_node):
+    instructions = 0
+    node = starting_node
+
+    while node[2] != 'Z':
+        node = mapping[node][0] if directions[instructions % len(directions)] == "L" else mapping[node][1]
+        instructions += 1
+
+    return instructions
+
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+
+lengths = [length_to_znode(node) for node in nodes]
+
+lcm_length = 1
+for length in lengths:
+    lcm_length = lcm(lcm_length, length)
+
+print(lcm_length)
